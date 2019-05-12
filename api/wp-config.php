de85dc70f7a2e42d2c6b67666fb7277097a7b568
Matheus Vilas Boas <mats.vilas@hotmail.com>
2019-05-11 22:08:48 -0300

    bla

--- api/wp-config.php
+++ api/wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'biblioteca');
 
 /** MySQL database username */
-define('DB_USER', 'dev');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'dev');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'biblioteca-mysql');
